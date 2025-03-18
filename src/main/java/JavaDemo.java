7ecb57a554cd5d671544b770c0287ee0ba98523a
veerrajukakarla434 <kakarlaveerraju2010@gmail.com>
2025-03-18 09:28:53 +0530

    Veer: Java file added

---
+++ src/main/java/JavaDemo.java
@@ -0,0 +1,9 @@
+
+public class JavaDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Welcome to Java World March 2025");
+	}
+
+}
